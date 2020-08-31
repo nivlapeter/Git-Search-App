f023f848123009917201f5c6a8b08f9e93952df7
nivlapeter <nivlapeter.0549@gmail.com>
2020-08-31 03:00:57 +0300

    addition of components,directives.classes,services files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
+import { UserRepositoryComponent } from './user-repository/user-repository.component';
+import { UserFormComponent } from './user-form/user-form.component';
+import { DateCountPipe } from './date-count.pipe';
+import { ShadowDirective } from './shadow.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    UserRepositoryComponent,
+    UserFormComponent,
+    DateCountPipe,
+    ShadowDirective
   ],
   imports: [
     BrowserModule,
